18f8c70c5ebef55c3ebd2afa5881fdd3f10050a3
Maksim Faronov <faronov_m@mail.ru>
2021-11-15 00:12:26 -0500

    trhydgfrh

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const app = express();
 // serve static contents
 //app.use(express.static(path.join('static',__dirname)));
 
-app.use(express.static('./static'));
+app.use(express.static('./static/index.html'));
 
 // dynamic handling
 
